c484d0fcb31e154449c590e89caf4c0e75024ed6
Charles Chamberlain <charlespipin@gmail.com>
2015-01-17 22:11:15 -0600

    Added self.board and self.player

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ class Vertex:
 	# Formula: 1b Parity, 2b Win-State, 32b Board
     
 	def __init__(self):
+		# setup board and player
+		self.board = self.data[4:]
+		self.player = self.data[0]
 		
 		self.color = self.is_winning_state()
+
 		if not self.color == "0b00":
 		    self.data[1:3] = self.color
 		    return
-		
+
 		# setup children
     	self.children = self.get_children()
-		
+
+
 		# do something
 		
 		# assuming we have list of children done
 		
 		self.color = "0b0"
-		self.player = self.data[0]
 		for child in self.children:
 		    self.color = self.color[0] or child.data[self.player+1]
 		self.data[self.player+1] = self.color
