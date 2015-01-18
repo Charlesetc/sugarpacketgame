ece838b10b0fe8b029ac6a12fe2f2277f4406e7f
malachite-sprite <m4rch3r@gmail.com>
2015-01-17 22:00:53 -0600

    added color resolution to __init__

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 # The Main Model of the Sugar Packet Game.
 
 class Vertex:
-	board = "0b00000000000000000000000000000000000"
+	self.data = "0b00000000000000000000000000000000000"
 	# Formula: 1b Parity, 2b Win-State, 32b Board
     
 	def __init__(self):
-		self.all_next = []
+		
+		self.color = self.is_winning_state()
+		if not self.color == 00:
+		    self.data[1:3] = self.color
+		
+		# assuming we have list of children
+		self.children = self.get_children()
+		
+		self.color = 0
+		self.player = self.data[0]
+		for child in self.children:
+		    self.color = self.color or child.data[self.player+1]
+		self.data[ ] = self.color
+		    
+    
+    def is_winning_state(self):
+        return 0
     
 	def __eq__(self, other):
-		return self.board == other.board
+		return self.data == other.data
     
     def __ne__(self, other):
-		return not self.board == other.board
+		return not self.data == other.data
     
     def __str__(self):
-        return self.board
+        return self.data
     
 	def player(self):
-		return self.board[0]
+		return self.data[0]
     
 
 
