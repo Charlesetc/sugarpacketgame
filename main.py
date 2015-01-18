72f3af46d5e10fb6f843d0eadabc5e1ab90ec572
malachite-sprite <m4rch3r@gmail.com>
2015-01-17 20:50:42 -0600

    made pythonic

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ class Vertex:
 	board = "0b00000000000000000000000000000000000"
 	# Formula: 1b Parity, 2b Win-State, 32b Board
 
-
+    
 	def __init__(self):
-		self.all_next
-
-	def equal_to(self, other):
-		self.board == other.board	
-
+		self.all_next = []
+    
+    
+	def __eq__(self, other):
+		return self.board == other.board
+    
+    
+    def __ne__(self, other):
+		return not self.board == other.board
+    
+    
+    def __str__(self):
+        return self.board
+    
 	def player(self):
-		self.board[0]
+		return self.board[0]
+    
+
+if __name__ == "__main__":
+    print "Hello world!"
